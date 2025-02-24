4ae0e494af89e9f2ea80605fd9ee414168ca6cc4
Steven Meyer <steven.a.meyer23@outlook.com>
2025-02-24 18:50:48 -0500

    Initial commit for Accessing List Elements - Negative Index.py

---
+++ Accessing List Elements - Negative Index.py
@@ -0,0 +1,5 @@
+shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
+last_element = shopping_list[-1]
+index5_element = shopping_list[5]
+print(index5_element)
+print(last_element)
