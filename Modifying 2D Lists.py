dcb30d98ba126b9ae8934d9333d20391e19c1aa6
Steven Meyer <steven.a.meyer23@outlook.com>
2025-02-25 15:19:31 -0500

    Modified list in Modifying 2D Lists.py

--- Modifying 2D Lists.py
+++ Modifying 2D Lists.py
@@ -1,8 +1,4 @@
-incoming_class = [
-    ["Kenny", "American", 9],
-    ["Tanya", "Ukrainian", 9],
-    ["Madison", "Indian", 7]
-]
+incoming_class = [["Kenny", "American", 9], ["Tanya", "Ukrainian", 9], ["Madison", "Indian", 7]]
 print(incoming_class)
 
 incoming_class[2][2] = 8
